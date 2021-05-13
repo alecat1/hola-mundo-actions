f61ad4f166456b0448d6865ed46c8689d3ea3c98
alecat1 <alejalopez42@hotmail.com>
2021-05-12 23:37:09 -0500

    karma.conf

--- karma.conf.js
+++ karma.conf.js
@@ -1,6 +1,13 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
+process.env.CHROME_BIN = require('puppeteer').executablePath()
+
+module.exports = function(config) {
+  config.set({
+    browsers: ['ChromeHeadless']
+  })
+}
 module.exports = function (config) {
   config.set({
     basePath: '',
